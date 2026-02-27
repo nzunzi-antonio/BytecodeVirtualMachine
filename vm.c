ed00c55bf11e7a210ce9062e8a945439c0742f91
nzunzi-antonio <nzunzi.antonio17@gmail.com>
2026-02-27 03:26:42 +0100

    feat: A Virtual Machine - Executing instructions - interpret run method

--- vm.c
+++ vm.c
@@ -9,6 +9,21 @@ void initVM() {
 void freeVM() {
 }
 
+static InterpretResult run() {
+#define READ_BYTE() (*vm.ip++)
+
+  for (;;) {
+    uint8_t instruction;
+    switch (instruction = READ_BYTE()) {
+      case OP_RETURN: {
+        return INTERPRET_OK;
+      }
+    }
+  }
+
+#undef READ_BYTE
+}
+
 InterpretResult interpret(Chunk* chunk) {
   vm.chunk = chunk;
   vm.ip = vm.chunk->code;
