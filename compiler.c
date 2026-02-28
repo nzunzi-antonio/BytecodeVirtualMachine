dec2f9e059a7d17c8fdbe3cd2b4f9d73a9e20a95
nzunzi-antonio <nzunzi.antonio17@gmail.com>
2026-02-28 19:19:53 +0100

    feat: update parsePrece function

--- compiler.c
+++ compiler.c
@@ -203,7 +203,14 @@ ParseRule rules[] = {
 };
 
 static void parsePrecedence(Precedence precedence) {
-  // What goes here?
+  advance();
+  ParseFn prefixRule = getRule(parser.previous.type)->prefix;
+  if (prefixRule == NULL) {
+    error("Expect expression.");
+    return;
+  }
+
+  prefixRule();
 }
 
 static ParseRule* getRule(TokenType type) {
