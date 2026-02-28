5081545cfe74efe8e14b676a1fee0b7b77c595c8
nzunzi-antonio <nzunzi.antonio17@gmail.com>
2026-02-28 04:32:12 +0100

    feat: implement function checkKeyword

--- scanner.c
+++ scanner.c
@@ -98,6 +98,16 @@ static void skipWhitespace() {
   }
 }
 
+static TokenType checkKeyword(int start, int length,
+    const char* rest, TokenType type) {
+  if (scanner.current - scanner.start == start + length &&
+      memcmp(scanner.start + start, rest, length) == 0) {
+    return type;
+  }
+
+  return TOKEN_IDENTIFIER;
+}
+
 static TokenType identifierType() {
   return TOKEN_IDENTIFIER;
 }
