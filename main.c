e85706b1870173dd026857006171fa40b14eada8
nzunzi-antonio <nzunzi.antonio17@gmail.com>
2026-02-28 03:06:27 +0100

    feat: Implement Scanner - Spinning Up the Interpreter - replace main

--- main.c
+++ main.c
@@ -6,19 +6,15 @@
 int main(int argc, const char* argv[]) {
   initVM();
 
-  Chunk chunk;
-  initChunk(&chunk);
+    if (argc == 1) {
+    repl();
+  } else if (argc == 2) {
+    runFile(argv[1]);
+  } else {
+    fprintf(stderr, "Usage: clox [path]\n");
+    exit(64);
+  }
 
-  int constant = addConstant(&chunk, 1.2);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
-  writeChunk(&chunk, OP_NEGATE, 123);
-
-  writeChunk(&chunk, OP_RETURN, 123);
-
-  disassembleChunk(&chunk, "test chunk");
-  interpret(&chunk);
   freeVM();
-  freeChunk(&chunk);
   return 0;
 }
