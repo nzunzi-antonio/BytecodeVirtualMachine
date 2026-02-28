86dd11640f4ecbecae3856514c637aeb0f6b230b
nzunzi-antonio <nzunzi.antonio17@gmail.com>
2026-02-28 18:28:50 +0100

    feat: implment advance function in compiler

--- compiler.c
+++ compiler.c
@@ -4,6 +4,17 @@
 #include "compiler.h"
 #include "scanner.h"
 
+static void advance() {
+  parser.previous = parser.current;
+
+  for (;;) {
+    parser.current = scanToken();
+    if (parser.current.type != TOKEN_ERROR) break;
+
+    errorAtCurrent(parser.current.start);
+  }
+}
+
 bool compile(const char* source, Chunk* chunk) {
   initScanner(source);
   advance();
