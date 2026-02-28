6d18974be813f7a56915c542abd2ba40db9b2d51
nzunzi-antonio <nzunzi.antonio17@gmail.com>
2026-02-28 19:06:39 +0100

    feat: implement grouping function

--- compiler.c
+++ compiler.c
@@ -95,6 +95,11 @@ static void endCompiler() {
   emitReturn();
 }
 
+static void grouping() {
+  expression();
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
+}
+
 static void number() {
   double value = strtod(parser.previous.start, NULL);
   emitConstant(value);
