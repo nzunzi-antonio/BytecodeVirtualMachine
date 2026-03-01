d897a033d853b20692a95c132178032b14074692
nzunzi-antonio <nzunzi.antonio17@gmail.com>
2026-03-01 03:57:04 +0100

    feat: add debug print on endcompiler

--- compiler.c
+++ compiler.c
@@ -116,6 +116,12 @@ static void emitConstant(Value value) {
 
 static void endCompiler() {
   emitReturn();
+
+#ifdef DEBUG_PRINT_CODE
+  if (!parser.hadError) {
+    disassembleChunk(currentChunk(), "code");
+  }
+#endif
 }
 
 static void expression();
